e710e863f61381fd2e06b82680dc301b50b1c8dd
Stefan Röhrbein <roehrbein@dial.de>
2017-02-10 13:39:09 +0100

    some code examples from go by example and go web by examples

---
+++ web/staticfiles/staticfiles.go
@@ -0,0 +1,11 @@
+package main
+
+import "net/http"
+
+func main() {
+	fileServer := http.FileServer(http.Dir("assets/"))
+
+	http.Handle("/static/", http.StripPrefix("/static/", fileServer))
+
+	http.ListenAndServe(":8080", nil)
+}
